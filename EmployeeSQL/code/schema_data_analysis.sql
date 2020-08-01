0ce66f8155b69c1026139f4a0591bafeef40008f
Jay Sueno <jay.sueno@gmail.com>
2020-08-01 02:02:04 -0700

    analysis task 5 complete

--- EmployeeSQL/code/schema_data_analysis.sql
+++ EmployeeSQL/code/schema_data_analysis.sql
@@ -10,6 +10,24 @@ select first_name, last_name, hire_date
 from employees
 where hire_date between '1986-01-01;' and '1986-12-31'; 
 
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+from departments
+inner join dept_manager on dept_manager.dept_no=departments.dept_no
+inner join employees on dept_manager.emp_no=employees.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+inner join dept_emp as x on x.emp_no=e.emp_no
+inner join departments as d on d.dept_no=x.dept_no;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees 
+where first_name='Hercules'
+and last_name like 'B%';
+
 select * from employees
 
 -- Perform an INNER JOIN on the two tables
